1028fe983dd646957b390435c6018eec24d3f256
SantiagoSantafe <andressasi@unisabana.edu.co>
2023-12-01 18:05:18 -0500

    Update dependencies and fix App component JSX syntax

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 function App() {
-  <><h1>Computer Vision!</h1>
-    //Un cuadro de texto para escribir la dirección URL de la imagen que se va a analizar o la solicitud de imagen que se va a generar
-    <input type="text" id="inputImage" name="inputImage"/>
-    //Un botón para desencadenar el análisis de imágenes y otro para desencadenar la generación de imágenes
-    <button id="analyzeButton">Analyze image</button>
-    <button id="generateButton">Generate image</button>
-    
+  return (
+    <>
+      <h1>Computer Vision!</h1>
+      <input type="text" id="inputImage" name="inputImage"/>
+      <button id="analyzeButton">Analyze image</button>
+      <button id="generateButton">Generate image</button>
     </>
+  );
 }
 
 export default App;
